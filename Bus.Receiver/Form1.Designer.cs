9ce77d7c440286c51776831e6bf9db820d4689be
Miroslav Holec <mirek.holec@hotmail.com>
2018-03-17 09:42:53 +0100

    LAB 6 - Azure Service Bus

---
+++ Bus.Receiver/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Bus.Receiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReceiveButton = new System.Windows.Forms.Button();
+            this.MessageText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 63);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RECEIVER";
+            // 
+            // ReceiveButton
+            // 
+            this.ReceiveButton.Location = new System.Drawing.Point(41, 109);
+            this.ReceiveButton.Name = "ReceiveButton";
+            this.ReceiveButton.Size = new System.Drawing.Size(175, 48);
+            this.ReceiveButton.TabIndex = 4;
+            this.ReceiveButton.Text = "Získat zprávu";
+            this.ReceiveButton.UseVisualStyleBackColor = true;
+            this.ReceiveButton.Click += new System.EventHandler(this.ReceiveButton_Click);
+            // 
+            // MessageText
+            // 
+            this.MessageText.Location = new System.Drawing.Point(41, 181);
+            this.MessageText.Multiline = true;
+            this.MessageText.Name = "MessageText";
+            this.MessageText.Size = new System.Drawing.Size(704, 176);
+            this.MessageText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MessageText);
+            this.Controls.Add(this.ReceiveButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ReceiveButton;
+        private System.Windows.Forms.TextBox MessageText;
+    }
+}
+
