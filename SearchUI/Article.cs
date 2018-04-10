e0be31c3a90c2a497695d3dcb19bce81510c42ce
Miroslav Holec <mirek.holec@hotmail.com>
2018-04-10 09:22:24 +0200

    LAB 8 - Azure Search UI Client

---
+++ SearchUI/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchUI
+{
+    public class Article
+    {
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Text { get; set; }
+    }
+}
